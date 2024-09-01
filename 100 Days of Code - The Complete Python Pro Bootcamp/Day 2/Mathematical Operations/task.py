915c4c8cbbffef099541082d1751759b98a44456
Sabeen <khannssabeen@gmail.com>
2024-09-01 22:12:21 +0200

    add pycharm 100 days bootcamp challenge

---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Mathematical Operations/task.py
@@ -0,0 +1,5 @@
+print("My age: " + str(12))
+print(3 * 3 + 3 / 3 - 3) # 7
+print(int((3 * 3 + 3 / 3 - 7)))
+print(2 ** 3)
+
---
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,7 @@
+len("12345")
+print(type("abc"))
+print(type(1))
+print(type(3.14))
+print(type(False))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
